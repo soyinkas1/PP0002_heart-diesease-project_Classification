a64d1f3697b5d96c5c8a6398ff518f56501b0eca
unknown <Soyinka Sowoolu>
2024-06-21 02:57:42 +0100

    updated

--- __init__.py
+++ __init__.py
@@ -1,27 +1,27 @@
-from flask import Flask, render_template
-from flask_bootstrap import Bootstrap
-from flask_mail import Mail
-from flask_moment import Moment
-from flask_sqlalchemy import SQLAlchemy
-from config import Config
+# from flask import Flask, render_template
+# from flask_bootstrap import Bootstrap
+# from flask_mail import Mail
+# from flask_moment import Moment
+# from flask_sqlalchemy import SQLAlchemy
+# from config import Config
 
-bootstrap = Bootstrap()
-mail = Mail()
-moment = Moment()
-db = SQLALchemy()
-config = Config()
+# bootstrap = Bootstrap()
+# mail = Mail()
+# moment = Moment()
+# db = SQLALchemy()
+# config = Config()
 
-def create_app(config_name):
-    app = Flask(__name__)
-    app.config.from_object(config)
-    config.init_app(app)
+# def create_app(config_name):
+#     app = Flask(__name__)
+#     app.config.from_object(config)
+#     config.init_app(app)
 
-    bootstrap.init_app(app)
-    mail.init_app(app)
-    moment.init_app(app)
-    db.init_app(app)
+#     bootstrap.init_app(app)
+#     mail.init_app(app)
+#     moment.init_app(app)
+#     db.init_app(app)
 
-    # attach routes and custom error pages here
+#     # attach routes and custom error pages here
 
-    return app
+#     return app
 
---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, redirect, render_template, url_for
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+
+    return render_template('index.html', page_content='Testing')
+
+if __name__ == '__main__':
+    app.run(debug=True)
+    
+
