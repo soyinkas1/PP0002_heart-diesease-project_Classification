620827d257efff0f8c769a18cab99a544e2586c8
unknown <soyinkas1@gmail.com>
2024-06-27 16:13:17 +0100

    update

--- config.py
+++ config.py
@@ -1,14 +1,16 @@
 import os
+from dotenv import load_dotenv
 
+load_dotenv()
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
-        SECRET_KEY = os.environ.get('SECRET_KEY')
-        MAIL_SERVER = os.environ.get('MAIL_SERVER')
-        MAIL_PORT = os.environ.get('MAIL_PORT')
-        MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS')
-        MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
-        MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+        SECRET_KEY = os.getenv('SECRET_KEY')
+        MAIL_SERVER = os.getenv('MAIL_SERVER')
+        MAIL_PORT = os.getenv('MAIL_PORT')
+        MAIL_USE_TLS = os.getenv('MAIL_USE_TLS')
+        MAIL_USERNAME = os.getenv('MAIL_USERNAME')
+        MAIL_PASSWORD = os.getenv('MAIL_PASSWORD')
 
 
         @staticmethod
