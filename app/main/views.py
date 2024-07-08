37c6c48f55c6fad123c1c91e270ead8db745789d
unknown <soyinkas1@gmail.com>
2024-07-08 17:47:31 +0100

    predict pipeline update

--- app/main/views.py
+++ app/main/views.py
@@ -3,6 +3,12 @@ from . import main
 from .forms import WebForm
 from .. import db
 from ..db_models import HeartPredictions
+from app.main.pipeline.prediction_pipeline import CustomData, PredictPipeline
+from app.main.config.configuration import ConfigurationManager
+
+
+config = ConfigurationManager()
+predict_config = config.get_prediction_pipeline_config()
 
 @main.route('/', methods=['GET', 'POST'])
 def index():
@@ -17,8 +23,30 @@ def predict_datapoint():
     if form.validate_on_submit():
          
         data = CustomData(
-        return redirect(url_for('.index'))
-    
-    
-    return render_template('index.html',
-                           form=form)
+            age=form.age.data,
+            sex=form.sex.data,
+            cp=form.cp.data,
+            trestbpscp=form.tresbpscp.data,
+            chol=form.chol.data,
+            fbs=form.fbs.data,
+            restecg=form.restecg.data,
+            thalach=form.thalach.data,
+            exang=form.exang.data,
+            oldpeak=form.oldpeak.data,
+            slope=form.slope.data,
+            ca=form.ca.data,
+            thal=form.thal.age
+        )
+        pred_df=data.get_data_as_data_frame()
+        print(pred_df)
+
+                
+            
+        obj = PredictPipeline(config=predict_config)
+        predict = obj.predict(pred_df)
+
+        return render_template('results.html', predict=predict)
+
+    else:
+
+        return render_template('prediction.html', form=form)
