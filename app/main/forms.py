d99beac060e7c65f67b09cd46e6127674fca87ed
unknown <soyinkas1@gmail.com>
2024-07-10 21:12:41 +0100

    app  form update

--- app/main/forms.py
+++ app/main/forms.py
@@ -16,23 +16,22 @@ class WebForm(FlaskForm):
     config = ConfigurationManager()
     webform_config = config.get_webform_config
      
-    
-    
+        
     email = StringField("Email Address (text)", validators=[DataRequired()], 
                                           render_kw={"placeholder": "Provide the email address that the prediction will be sent to"})
     age = IntegerField('Age in years',default=0 ,validators=[InputRequired()])
-    sex = SelectField('Sex (1 = male; 0 = female)', choices=[('1', 'male'), ('0', 'female')], validate_choice=True)
-    cp = SelectField('Sex (1 = male; 0 = female)', choices=[('1', 'male'), ('0', 'female')], validate_choice=True)
+    sex = SelectField('Sex (1 = male; 0 = female)', choices=[(1, 'male'), (0, 'female')], validate_choice=True)
+    cp = SelectField('Chest Pain Type)', choices=[(1, 'typical angina'),(2, 'atypical angina'),(3, 'non-anginal pain'), (4, 'asymptomatic')], validate_choice=True)
     trestbps = IntegerField("Resting blood pressure (in mm Hg on admission to the hospital)", default=0 ,validators=[InputRequired()])
     chol = IntegerField("Serum cholestoral in mg/dl", default=0 ,validators=[InputRequired()])
-    fbs = SelectField('(Fasting blood sugar &gt; 120 mg/dl) (1 = true; 0 = false)', choices=[('1', 'male'), ('0', 'female')], validate_choice=True)
-    restecg = SelectField('Resting electrocardiographic results', choices=[('1', 'male'), ('0', 'female')], validate_choice=True)
+    fbs = SelectField('(Fasting blood sugar &gt; 120 mg/dl) (1 = True; 0 = False)', choices=[(1, 'True'), (0, 'False')], validate_choice=True)
+    restecg = SelectField('Resting electrocardiographic results', choices=[(0, '0'), (1, '1'), (2, '2')], validate_choice=True)
     thalach = IntegerField('Maximum heart rate achieved', default=0 ,validators=[InputRequired()])
-    exang = SelectField('Sex (1 = male; 0 = female)', choices=[('1', 'male'), ('0', 'female')], validate_choice=True)
+    exang = SelectField('Sex (1 = Yes; 0 = No)', choices=[(1, 'Yes'), (0, 'No')], validate_choice=True)
     oldpeak = FloatField('ST depression induced by exercise relative to rest', default=0.00 ,validators=[InputRequired()])
-    slope = SelectField('Sex (1 = male; 0 = female)', choices=[('1', 'male'), ('0', 'female')], validate_choice=True)
-    ca = SelectField('Sex (1 = male; 0 = female)', choices=[('1', 'male'), ('0', 'female')], validate_choice=True)
-    thal = SelectField('Sex (1 = male; 0 = female)', choices=[('1', 'male'), ('0', 'female')], validate_choice=True)
+    slope = SelectField('the slope of the peak exercise ST segment', choices=[(0, '0'), (1, '1'), (2, '2')], validate_choice=True)
+    ca = SelectField('Number of major vessels (0-3) colored by flourosopy', choices=[(0, '0'),(1, '1'),(2, '2'), (3, '3')], validate_choice=True)
+    thal = SelectField('1 = normal; 2 = fixed defect; 3 = reversable defect', choices=[(1, 'Normal'), (2, 'fixed defect'), (3, 'reversable defect')], validate_choice=True)
     submit = SubmitField('Submit', validators=[DataRequired()])
 
 
